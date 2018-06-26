b3ee5ae1266638743f6e8f41e9f74b42bfec7cae
keigorori <www.k544.com@gmail.com>
2018-06-26 13:40:04 +0900

    install.batを追加

---
+++ install.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+pushd "%~dp0"
+
+mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
+mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
+
+popd
+exit /B 0
