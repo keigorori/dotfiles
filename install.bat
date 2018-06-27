6de21d0c02f924d30ea4fd24dd7a33eb03aee740
keigorori <www.k544.com@gmail.com>
2018-06-27 16:24:13 +0900

    winではbashrcとvimrcを使わないように修正

--- install.bat
+++ install.bat
@@ -2,8 +2,8 @@
 setlocal
 pushd "%~dp0"
 
-mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
-mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+::mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
+::mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
 mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
 
 popd
