726b29d11c0b697937f804fc9c3bb326fb72d119
keigorori <www.k544.com@gmail.com>
2018-07-04 11:47:09 +0900

    .gitignoreを追加

--- install.bat
+++ install.bat
@@ -4,7 +4,9 @@ pushd "%~dp0"
 
 ::mklink %HOMEPATH%"\.bashrc" %HOMEPATH%"\dotfiles\.bashrc"
 ::mklink %HOMEPATH%"\.vimrc" %HOMEPATH%"\dotfiles\.vimrc"
+mklink %HOMEPATH%"\.vsvimrc" %HOMEPATH%"\dotfiles\.vsvimrc"
 mklink %HOMEPATH%"\.gitconfig" %HOMEPATH%"\dotfiles\.gitconfig"
+mklink %HOMEPATH%"\.gitignore" %HOMEPATH%"\dotfiles\.gitignore"
 
 popd
 exit /B 0
