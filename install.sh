27aca88562a2ce3e37ebdf79ef9e72f95a68eca7
keigorori <www.k544.com@gmail.com>
2019-03-01 13:27:47 +0900

    install.shで.gitignoreのリンク処理を追加

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-﻿#!/bin/sh
+#!/bin/sh
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.bashrc ~/.bashrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/.gitignore ~/.gitignore
