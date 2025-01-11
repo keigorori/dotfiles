0b72f50a40034b50318c0489e779cc9f35dcbc71
keigorori <www.k544.com@gmail.com>
2025-01-12 01:47:16 +0900

    zshに対応

---
+++ .zshrc
@@ -0,0 +1,22 @@
+#.zshrc
+
+## locale
+export LANG=en_US.UTF-8
+
+## homebrew
+export PATH=/opt/homebrew/bin:$PATH
+
+## zsh-completions
+if type brew &>/dev/null && [ -e $(brew --prefix)/share/zsh-completions ]; then
+    FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+    autoload -Uz compinit
+    compinit
+fi
+
+## git-prompt
+if [ -e "$HOME/.zsh/git-prompt.sh" ]; then
+    source "$HOME/.zsh/git-prompt.sh"
+    GIT_PS1_SHOWDIRTYSTATE=true
+    setopt PROMPT_SUBST
+    PS1='%F{green}%n@%m%f: %F{cyan}%~%f %F{red}$(__git_ps1 "(%s)")%f\$ '
+fi
--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
-ln -sf ~/dotfiles/.bashrc ~/.bashrc
+
+if [ "$SHELL" = "/bin/zsh" ]; then
+    ln -sf ~/dotfiles/.zshrc ~/.zshrc
+    if [ ! -e "$HOME/.zsh" ]; then mkdir "$HOME/.zsh"; fi
+    # git-promptのダウンロード
+    if [ ! -e "$HOME/.zsh/git-prompt" ]; then
+        echo "Downloading git-prompt"
+        curl -o ~/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
+    fi
+elif [ "$SHELL" = "/bin/bash" ]; then
+    ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+    ln -sf ~/dotfiles/.bashrc ~/.bashrc
+else
+    echo "Unknown shell"
+fi
+
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
